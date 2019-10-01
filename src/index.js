1f6b87b389b99da659b8906ae509d109c4896fab
phucnv282 <phucnguyenbn.9x@gmail.com>
2019-10-01 17:24:18 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const routes = require('./routes');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use('/', routes);
+
+app.set('port', 3001);
+
+const server = app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+module.exports = app;
